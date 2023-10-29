e012209871ba114a0b950fe70e53ba9b5a110daa
FabLetizia <fabio.letizia01@gmail.com>
2023-10-29 17:29:35 +0100

    Update JSONDataProcessor.java

--- src/JSONDataProcessor.java
+++ src/JSONDataProcessor.java
@@ -1,11 +1,12 @@
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class JSONDataProcessor implements DataProcessor{
     @Override
