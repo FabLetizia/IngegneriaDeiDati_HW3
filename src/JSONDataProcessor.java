d7c254f755e0ea5c73de79c8d06acacf6310089a
AlessandroPesare <ale.pesare@stud.uniroma3.it>
2023-10-29 18:40:06 +0100

    Update JSONDataProcessor.java

--- src/JSONDataProcessor.java
+++ src/JSONDataProcessor.java
@@ -2,16 +2,14 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.*;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class JSONDataProcessor implements DataProcessor{
+public class JSONDataProcessor implements DataProcessor {
     @Override
     public Document processHTMLTable(String htmlTable) {
         return null;
@@ -21,6 +19,10 @@ public class JSONDataProcessor implements DataProcessor{
     public Document processJSONData(File jsonFile) throws IOException {
         Gson gson = new Gson();
         Document luceneDoc = new Document();
+
+        // Estrai il nome del file (preso come ID del documento)
+        String documentID = jsonFile.getName();
+        luceneDoc.add(new StringField("documentID", documentID, Field.Store.YES));
 
         // Leggi il contenuto del file JSON
         try (BufferedReader reader = new BufferedReader(new FileReader(jsonFile))) {
@@ -37,18 +39,20 @@ public class JSONDataProcessor implements DataProcessor{
             JsonArray cellsArray = jsonObject.getAsJsonArray("cells");
 
             // Itera sugli oggetti JSON in "cells" e cerca il campo "cleanedText"
+            StringBuilder extractedText = new StringBuilder();
             for (JsonElement cellElement : cellsArray) {
                 JsonObject cellObject = cellElement.getAsJsonObject();
                 if (cellObject.has("cleanedText")) {
+
                     String cleanedText = cellObject.get("cleanedText").getAsString();
-                    luceneDoc.add(new TextField("cleanedText", cleanedText, Field.Store.YES));
+                    int columnID = cleanedText.hashCode();
+                    extractedText.append(cleanedText).append(" ");
+                    luceneDoc.add(new IntField("columnID", columnID, Field.Store.YES));
                 }
             }
+            luceneDoc.add(new TextField("extractedText", extractedText.toString(), Field.Store.YES));
         }
 
         return luceneDoc;
     }
-
-
-
 }
